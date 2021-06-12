9f0b0fa3c16d96bf6dab878702864e2a4dea6727
jessiiepeng <jessie.peng2010@gmail.com>
2021-06-12 12:53:41 -0400

    put google colab to vs code successfullygit add .

--- model.py
+++ model.py
@@ -44,12 +44,115 @@ test_images = test.iloc[:, 1:]
 #train_images: (297960, 784)
 
 #PREPROCESS THE DATA
-plt.figure()
-#reshape Series back into 28 x 28
-reshaped = train_images.iloc[0].values.reshape(28, 28)
-plt.imshow(reshaped)
-plt.colorbar()
-plt.grid(False)
-plt.show()
+# plt.figure()
+# #reshape Series back into 28 x 28
+# reshaped = train_images.iloc[0].values.reshape(28, 28)
+# plt.imshow(reshaped)
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+#Scale data all to range of 0 to 1
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
 
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images.iloc[i].values.reshape(28,28), cmap=plt.cm.binary)
+#     #iloc returns either series/dataframe, item() returns value
+#     plt.xlabel(class_names[train_labels.iloc[i].item()]) 
+# plt.show()
+
+#MODEL TIME
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(26) #26 output nodes
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+#EVALUATING ACCURACY
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+#MAKING PREDICTIONS
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
+
+print(predictions[0])
+print(np.argmax(predictions[0]))
+print(test_labels.iloc[0].item())
+
+def plot_image(i, predictions_array, true_label, img):
+  #true_label, img = true_label.iloc[i].item(), img.iloc[i].values.reshape(28,28)
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  # true_label = true_label.iloc[i].item()
+  plt.grid(False)
+  plt.xticks(range(26))
+  plt.yticks([])
+  thisplot = plt.bar(range(26), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+  # Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+# num_rows = 5
+# num_cols = 3
+# num_images = num_rows*num_cols
+# plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+# for i in range(num_images):
+#   plt.subplot(num_rows, 2*num_cols, 2*i+1)
+#   plot_image(i, predictions[i], test_labels.iloc[i].item(),
+#              test_images.iloc[i].values.reshape(28,28))
+#   plt.subplot(num_rows, 2*num_cols, 2*i+2)
+#   plot_value_array(i, predictions[i], test_labels.iloc[i].item())
+# plt.tight_layout()
+# plt.show()
+
+
+#USE THE TRAINED MODEL!!
+# i = 1200
+
+# #Get image from test datset
+# img = test_images.iloc[i]
+# img = (np.expand_dims(img,0))
+# predictions_single = probability_model.predict(img)
+# # print(predictions_single)
+
+# print(test_labels.iloc[i].item())
+# print("Expected ", class_names[test_labels.iloc[i].item()])
+# print(np.argmax(predictions_single[0]))
+# print("Got ", class_names[np.argmax(predictions_single[0])])
+
+# plot_value_array(i, predictions_single[0], test_labels.iloc[i].item())
+# _ = plt.xticks(range(26), class_names, rotation=45)
